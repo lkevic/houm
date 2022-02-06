b31c0ce69cf09c02d13117a70cb6350dc96cdaea
Leandro Kevicky <lkevicky@globaltask.net>
2022-02-06 15:47:34 -0300

    Se eliminan archivos no necesarios.

--- houmers/validators.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-
-def decimal_degrees(value):
-    if not -180.0 <= value <= 180.0:
-        raise serializers.ValidationError('This field must be decimal degrees.')
